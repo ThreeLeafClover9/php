64787cc9a4f8cde96c00da717e80388f21c51a66
clover <threeleafclover9@gmail.com>
2023-06-29 23:28:53 +0900

    Feat basic syntax

---
+++ BasicSyntax/hello_world.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Comments
+ */
+
+// Single line Comments
+
+# Single line Comments
+
+echo 'Hello, world';  // Print 'Hello, world'
+
+exit;  // Terminate script
+
+die;  // Terminate script
+
+print 'Hello, wrold';  // Print 'Hello, world'
